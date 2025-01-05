980f9ae8a12af2d592f6b748be7a6afb8dccaf93
Alesander Kus <79520681+alexanderKus@users.noreply.github.com>
2025-01-05 16:55:57 +0100

    works

--- routes/invoice.js
+++ routes/invoice.js
@@ -14,18 +14,13 @@ router.post('/', async (req, res) => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.setContent(html);
-        await page.emulateMediaType('screen');
-
-        const pdfBuffer = await page.pdf({
-            format: 'A4',
-            printBackground: true,
-        });
-
+        const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true, });
+        const buffer = Buffer.from(pdfBuffer)
         await browser.close();
         
         res.setHeader('Content-Disposition', 'attachment; filename="output.pdf"');
         res.setHeader('Content-Type', 'application/pdf');
-        res.send(pdfBuffer);
+        res.send(buffer);
     } catch(error) {
         console.error('Error generating PDF:', error);
         res.status(500).send('Error generating PDF');
