a0d10e097aea96d10bed768414583c41fcf39f90
Alesander Kus <79520681+alexanderKus@users.noreply.github.com>
2025-01-06 20:32:31 +0100

    Update invoice.model.js

--- models/invoice.model.js
+++ models/invoice.model.js
@@ -1,6 +1,20 @@
 class Invoice {
-    constructor(invoiceNumber) {
+    constructor(
+        invoiceNumber, 
+        issueDate, 
+        salesDate, 
+        dueDate, 
+        payment,
+        seller,
+        buyer
+    ) {
         this.invoiceNumber = invoiceNumber
+        this.issueDate = issueDate
+        this.salesDate = salesDate
+        this.dueDate = dueDate
+        this.payment = payment
+        this.seller = seller
+        this.buyer = buyer
     }
 }
 
