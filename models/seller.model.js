d149b5333f3bfbe84fad1737fc3fc75febd1737f
Alesander Kus <79520681+alexanderKus@users.noreply.github.com>
2025-01-06 20:32:29 +0100

    Create seller.model.js

---
+++ models/seller.model.js
@@ -0,0 +1,11 @@
+class Seller {
+    constructor(companyInfo, nipVat, account, bankName, swift) {
+    this.companyInfo = companyInfo
+    this.nipVat = nipVat
+    this.account = account
+    this.bankName = bankName
+    this.swift = swift
+    }
+}
+
+module.exports = Seller
