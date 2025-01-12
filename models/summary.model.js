afbd86fb31cbf63885fc240f4e89672e1323fbca
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2025-01-12 19:14:38 +0100

    Create summary.model.js

---
+++ models/summary.model.js
@@ -0,0 +1,13 @@
+class Summary {
+    constructor(
+        totalNetPrice,
+        vatAmount,
+        totalGrossPrice
+    ) {
+        this.totalNetPrice = totalNetPrice
+        this.vatAmount = vatAmount
+        this.totalGrossPrice = totalGrossPrice
+    }
+}
+
+module.exports = Summary
